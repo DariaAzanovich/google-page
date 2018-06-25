57f03570213291197d4b6861db5ab2c6488dcbc7
DariaAzanovich <azanovich.dasha@gmail.com>
2018-06-26 00:28:57 +0300

    Add Google page

---
+++ google.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function showAppsPopup(event) {
+  const popup = document.getElementById("popup");
+  popup.className = popup.className.includes("closed-popup") ? "popup opened-popup" : "popup closed-popup";
+  event.stopPropagation();
+}
+
+document.onclick = function(event) {
+  console.log('on doc click');
+  const popup = document.getElementById("popup");
+  if (!popup.contains(event.target)) {
+    popup.className = "popup closed-popup";
+  }
+}
